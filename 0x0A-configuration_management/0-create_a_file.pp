833991c32888f56c0090e35cee3b5dde441c4453
Mohammed <mrcloud.med@gmail.com>
2024-04-19 20:47:26 +0000

    Add comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,4 @@
+# This manifest will Create a file in /tmp with permission 0744, owner www-data, group www-data content "i love Puppet"
 file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
