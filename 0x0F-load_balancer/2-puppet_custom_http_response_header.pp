4951375f7794efe68ea6c94a00ab0273d814c97b
Mohammed <mrcloud.med@gmail.com>
2024-04-30 01:26:09 +0000

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# pp to install ngnix web server, add custum response header
+
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
