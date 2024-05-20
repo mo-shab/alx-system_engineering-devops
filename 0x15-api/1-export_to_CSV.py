a4ff92bb11c93e79afcc6b2e81c6232875115501
Mohammed <mrcloud.med@gmail.com>
2024-05-20 22:55:06 +0000

    Export CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module  to fetch data from an API"""
+
+import requests
+import sys
+import csv
+
+
+def export_cvs(employee_id):
+    """Get the todo list for a given employee id"""
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get("{}/users/{}".format(url, employee_id)).json()
+    todos = requests.get("{}/todos?userId={}".format(url, employee_id)).json()
+
+    with open("{}.csv".format(employee_id), "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([employee_id, user.get("username"),
+                             todo.get("completed"), todo.get("title")])
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py EMPLOYEE_ID")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("EMPLOYEE_ID must be an integer.")
+        sys.exit(1)
+
+    export_cvs(employee_id)
