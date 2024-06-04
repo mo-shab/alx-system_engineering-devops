81be6645d82b6394338a803d241b6031ac973aab
Mohammed <mrcloud.med@gmail.com>
2024-06-04 23:49:08 +0000

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to return number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that return number of Reddit subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'myApp/0.0.1'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
