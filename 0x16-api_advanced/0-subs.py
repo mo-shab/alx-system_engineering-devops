7635760884805aa897dbdc527b1af7bb1e1b7105
Mohammed <mrcloud.med@gmail.com>
2024-06-04 23:51:58 +0000

    pycodetest

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Function that return number of Reddit subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'myApp/0.0.1'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
